c01626ed54ae85496e937c1d7e1df636b1ce085e
SevenImm <angelcardenasptx@gmail.com>
2023-10-29 15:00:18 -0500

    Added main body layout

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,2 @@
-var weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
-const key = 0c6ba43fa0b2c135fd4db50b24680937;
+var weatherAPI = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
+const APIkey = '0c6ba43fa0b2c135fd4db50b24680937';
